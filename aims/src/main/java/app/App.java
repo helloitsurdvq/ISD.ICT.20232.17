fcbfa4a968f9f6e098b37b35e6d3008458e6698d
arinatei <158471042+arinatei@users.noreply.github.com>
2024-06-08 16:52:40 +0700

    commit

--- aims/src/main/java/app/App.java
+++ aims/src/main/java/app/App.java
@@ -4,19 +4,54 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+import utils.Configs;
+import services.home.HomeScreenService;
 
 import java.io.IOException;
 
 public class App extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        final String appName = "AIMS";
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/view/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.setTitle(appName);
-        stage.show();
+    public void start(Stage primaryStage) {
+        try {
+            StackPane root = (StackPane) FXMLLoader.load(getClass().getResource(Configs.SPLASH_SCREEN_PATH));
+
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+            FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), root);
+            fadeIn.setFromValue(0);
+            fadeIn.setToValue(1);
+            fadeIn.setCycleCount(1);
+
+            FadeTransition fadeOut = new FadeTransition(Duration.seconds(1), root);
+            fadeOut.setFromValue(1);
+            fadeOut.setToValue(0);
+            fadeOut.setCycleCount(1);
+
+            fadeIn.play();
+            fadeIn.setOnFinished((e) -> {
+                fadeOut.play();
+            });
+
+            fadeOut.setOnFinished((e) -> {
+                try {
+                    HomeScreenService homeHandler = new HomeScreenService(primaryStage, Configs.HOME_PATH);
+                    homeHandler.setScreenTitle("Home Screen");
+                    homeHandler.setImage();
+                    homeHandler.show();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
