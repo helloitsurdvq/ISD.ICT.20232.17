0a5d18d0a2daa8a5c25c02d08dd25828d7afe3ef
Tai Le <161448595+blackicecoffee@users.noreply.github.com>
2024-06-11 16:45:30 +0700

    add media and cart controller

---
+++ aims/src/main/java/controller/MediaController.java
@@ -0,0 +1,40 @@
+package controller;
+
+import model.media.Media;
+import model.media.Book;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.SQLException;
+
+public class MediaController extends BaseController {
+    Media media;
+
+    public MediaController() {
+        media = new Media();
+    }
+
+    public MediaController(Media media) {
+        this.media = media;
+    }
+
+    public ObservableList<Media> getAllMedia() throws SQLException {
+        return FXCollections.observableArrayList(media.getAllMedia());
+    }
+
+    public Media getMediaById(int id) throws SQLException {
+        return media.getMediaById(id);
+    }
+
+    public void saveMedia(Media media) throws SQLException {
+        media.save();
+    }
+
+    public void deleteMediaById(int id) throws SQLException {
+        media.delete(id);
+    }
+
+    public Book getBookMediaById(int id) throws SQLException {
+        return new Book().getMediaById(id);
+    }
+}
