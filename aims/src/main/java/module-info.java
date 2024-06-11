216f503ae8e42e69f87f381577a89bae04c37a88
Viet Quang <93778662+helloitsurdvq@users.noreply.github.com>
2024-06-12 00:01:59 +0700

    hotfix: payment res cannot display

--- aims/src/main/java/module-info.java
+++ aims/src/main/java/module-info.java
@@ -1,19 +1,31 @@
-//module com.aims {
-//    requires javafx.controls;
-//    requires javafx.fxml;
-//
-//
-//    opens com.aims to javafx.fxml;
-//    exports com.aims;
-//}
-
 module main {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.logging;
+    requires java.sql;
+    requires java.datatransfer;
+    requires java.desktop;
+    requires javafx.web;
+    requires org.xerial.sqlitejdbc;
 
     exports controller;
     exports app;
+    exports services;
 
     opens app to javafx.fxml;
-    opens controller to javafx.fxml;
+    opens controller to javafx.fxml, java.sql;
+    opens services to javafx.fxml, javafx.web, java.desktop;
+    opens services.home to javafx.fxml;
+    opens services.invoice to javafx.fxml;
+    opens services.manage to javafx.fxml;
+    opens services.cart to javafx.fxml;
+    opens services.manage.media to javafx.fxml;
+    opens services.manage.media.detail to javafx.fxml;
+    opens services.manage.media.form to javafx.fxml;
+    opens services.manage.order to javafx.fxml;
+    opens services.payment to javafx.fxml;
+    opens services.shipping to javafx.fxml;
+    opens model to javafx.base;
+    opens model.order to javafx.base;
+    opens model.media to javafx.base;
 }
