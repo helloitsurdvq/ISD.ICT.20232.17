d13bf7fd7d5420f9b9d789f686e335f1bd6238c7
Tai Le <161448595+blackicecoffee@users.noreply.github.com>
2024-06-15 18:21:12 +0700

    fix add to cart value

--- aims/src/main/java/services/home/HomeService.java
+++ aims/src/main/java/services/home/HomeService.java
@@ -50,7 +50,7 @@ public class HomeService extends FXMLScreenService {
                 // if media already in cart then we will increase the quantity by 1 instead of create the new cartMedia
                 CartMedia mediaInCart = home.getBController().checkMediaInCart(media);
                 if (mediaInCart != null) {
-                    mediaInCart.setQuantity(mediaInCart.getQuantity() + 1);
+                    mediaInCart.setQuantity(mediaInCart.getQuantity() + spinnerChangeNumber.getValue());
                 } else {
                     CartMedia cartMedia = new CartMedia(media, cart, spinnerChangeNumber.getValue(), media.getPrice());
                     cart.getListMedia().add(cartMedia);
